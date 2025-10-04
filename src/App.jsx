2f5954087f32f84c72d539fe0ae0d6331fb4a6df
LahiruDS <lahiruddesilva2022@gmail.com>
2025-10-04 19:27:57 +0530

    tailwind-css-1✔

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,23 @@
 import { useState } from 'react'
 import './App.css'
 import ProductCard from './components/productCard'
+import Header from './components/header'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <ProductCard name="Laptop" price="LKR: 10,000" image="https://www.bhphotovideo.com/images/images2500x2500/hp_28k88ut_aba_pb440g8_i7_1165g7_16_512_32_ix_w10p_14_1601641.jpg"/>
+    <div className="w-[600px] h-[600px] border bg-gray-400 flex justify-center items-center">
+      <div className="w-[500px] h-[500px]  bg-yellow-200 flex flex-col justify-center items-center">
+        <div className="w-[100px] h-[100px]  bg-red-500 fixed left-[500px] top-[500px]"></div>
+        <div className="w-[100px] h-[100px]  bg-green-500 absolute left-[500px]"></div>
+        <div className="w-[100px] h-[100px]  bg-blue-600"></div>
+        <div className="w-[100px] h-[100px]  bg-pink-500"></div>
+      </div>
 
-    <ProductCard name="Keyboard" price="LKR: 1,000" image="https://m.media-amazon.com/images/I/71DYlRN51tL._AC_SL1500_.jpg"/>
-
-    <ProductCard name="Mouse" price="LKR: 500" image="https://images-na.ssl-images-amazon.com/images/I/61QXVSF6bLL._AC_SL1500_.jpg"/>
-
-    <ProductCard name="Machine Case" price="LKR: 11,500" image="https://tse2.mm.bing.net/th/id/OIP.HzwkI94SR9Q_ohFhunOx9AHaHa?pid=Api&P=0&h=220"/>
-
-    <ProductCard name="Monitor(Used)" price="LKR: 9,500" image="https://m.media-amazon.com/images/I/71SMLQItkzL.jpg"/>
+    </div>
+    
     </>
   )
 }
---
+++ src/components/userData.jsx
@@ -0,0 +1,12 @@
+export default function UserData(){
+    return(
+        <div>
+                <img src="https://static.vecteezy.com/system/resources/previews/019/787/018/large_2x/shopping-cart-icon-shopping-basket-on-transparent-background-free-png.png" width="30px"/>
+                <span>Cart(0)</span>
+                <br/><br/>
+
+                <img src="https://static.vecteezy.com/system/resources/previews/019/879/186/original/user-icon-on-transparent-background-free-png.png" width="30px"/>
+                <span>Lahiru De Silva</span>
+        </div>
+    )
+}
